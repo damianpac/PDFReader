37bf19e08a6bfe6f5a5e3801f9a5f174e7cecece
Damian Pacholec <damian.pacholec@outlook.com>
2019-02-26 22:57:36 +0100

    Program.cs

---
+++ PDFReader/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+
+namespace PDFReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Damian";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PDF Files(*.pdf)|*.pdf";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string path = ofd.FileName.ToString();
+                textBox1.Text = path;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PDDocument doc = PDDocument.load(textBox1.Text);
+            PDFTextStripper stripper = new PDFTextStripper();
+            richTextBox1.Text = (stripper.getText(doc));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.DefaultExt = "*.txt";
+            saveFileDialog1.Filter = "TXT File|*.txt";
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+                saveFileDialog1.FileName.Length > 0)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+    }
+}
